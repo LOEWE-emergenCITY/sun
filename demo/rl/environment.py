595d100c9488afc7083540dc4b39aae196b613aa
Kai Cui <kai.cui@bcs.tu-darmstadt.de>
2021-11-02 16:17:54 +0100

    added RLlib docker container with application skeleton

---
+++ demo/rl/environment.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+import time
+import gym
+import ray
+import numpy as np
+from ray.rllib.agents.ppo import ppo
+from ray.tune import register_env
+
+
+class CoreGazeboEnv(gym.Env):
+    def __init__(self):
+        self.curr_time = None
+        self.t = 0
+
+        self.observation_space = gym.spaces.Box(-1, 1, shape=(2,), dtype=float)
+        self.action_space = gym.spaces.Box(-1, 1, shape=(2,), dtype=float)
+
+        self.reset()
+
+    def reset(self):
+        # TODO reset the entire simulation
+        physical_state = self.poll_gazebo_state()
+        network_state = self.poll_network_state()
+        return self.get_observation(physical_state, network_state)
+
+    def send_actions(self, action):
+        pass  # TODO send the flight controller the desired actions for the next time step
+
+    def poll_gazebo_state(self):
+        pass  # TODO poll gazebo container information
+
+    def poll_network_state(self):
+        pass  # TODO poll core container information
+
+    def get_observation(self, physical_state, network_state):
+        return np.array(self.observation_space.sample())  # TODO
+
+    def get_reward(self, physical_state, network_state):
+        return 0  # TODO
+
+    def get_done(self):
+        return self.t >= 50
+
+    def step(self, action):
+        self.send_actions(action)
+        time.sleep(1)
+        physical_state = self.poll_gazebo_state()
+        network_state = self.poll_network_state()
+
+        print("###########")
+        print(self.t)
+        print(physical_state)
+        print(network_state)
+        print("###########", flush=True)
+
+        obs = self.get_observation(physical_state, network_state)
+        reward = self.get_reward(physical_state, network_state)
+        done = self.get_done()
+        self.t += 1
+        return obs, reward, done, {}
+
+    def render(self, mode="human"):
+        pass
+
+
+if __name__ == '__main__':
+    def env_creator(env_config=None):
+        return CoreGazeboEnv()
+
+    # env_creator().step(None)
+
+    ray.init(local_mode=True)
+    register_env("Env-v0", env_creator)
+    trainer = ppo.PPOTrainer(env="Env-v0", config={
+        "num_gpus": 0,
+        'num_workers': 1,
+        "num_envs_per_worker": 1,
+        "gamma": 0.99,
+        # "entropy_coeff": 0.01,
+        "clip_param": 0.3,
+        "kl_target": 0.01,
+        "normalize_actions": True,
+        # "no_done_at_end": True,
+        "framework": 'torch',
+    })
+
+    logs = []
+    for iteration in range(5):
+        log = trainer.train()
+        print("Loop {} mean {} ent {}".format(iteration, log['episode_reward_mean'],
+                                                      log['info']['learner']['default_policy']['learner_stats']['entropy']))
+        if iteration % 50 == 0:
+            checkpoint = trainer.save()
+            trainer.load_checkpoint(checkpoint)
+        logs.append(log)
