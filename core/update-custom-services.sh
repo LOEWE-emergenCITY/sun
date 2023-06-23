45d8869dab353a46ea7d12771c7032c7b6d840c8
Lars Baumgaertner <1264131+gh0st42@users.noreply.github.com>
2023-06-23 08:39:20 +0200

    used dtn7-showroom as base for core image, added dtn7zero nodes to scenario

--- core/update-custom-services.sh
+++ core/update-custom-services.sh
@@ -2,19 +2,41 @@
 
 DIR="/shared/myservices"
 if [ -d "$DIR" ]; then  
-  echo "Adding ${DIR} to custom services dir of core"
-  echo custom_services_dir = /root/.core/myservices,/shared/myservices >> /etc/core/core.conf
+  # check if custom_services_dir already added to core.conf
+  grep -q "^custom_services_dir" /etc/core/core.conf
+  if [ $? -eq 0 ]; then
+    echo "custom_services_dir already added to core.conf"
+  else    
+    echo "Adding ${DIR} to custom services dir of core"
+    echo custom_services_dir = /root/.core/myservices,/shared/myservices >> /etc/core/core.conf
+  fi
 else
   ###  Control will jump here if $DIR does NOT exists ###
-  echo custom_services_dir = /root/.core/myservices >> /etc/core/core.conf
+  grep -q "^custom_services_dir" /etc/core/core.conf
+  if [ $? -eq 0 ]; then
+    echo "custom_services_dir already added to core.conf"
+  else
+    echo custom_services_dir = /root/.core/myservices >> /etc/core/core.conf
+  fi
 fi
 
 
 DIR="/shared/custom_services"
 if [ -d "$DIR" ]; then  
-  echo "Adding ${DIR} to custom services dir of core"
-  echo custom_config_services_dir = /root/.coregui/custom_services,/shared/custom_services >> /etc/core/core.conf
+  # check if custom_config_services_dir already added to core.conf
+  grep -q "^custom_config_services_dir" /etc/core/core.conf
+  if [ $? -eq 0 ]; then
+    echo "custom_config_services_dir already added to core.conf"
+  else
+    echo "Adding custom_config_services_dir to core.conf"
+    echo custom_config_services_dir = /root/.coregui/custom_services,/shared/custom_services >> /etc/core/core.conf
+  fi  
 else
   ###  Control will jump here if $DIR does NOT exists ###
-  echo custom_config_services_dir = /root/.coregui/custom_services >> /etc/core/core.conf
+  grep -q "^custom_config_services_dir" /etc/core/core.conf
+  if [ $? -eq 0 ]; then
+    echo "custom_config_services_dir already added to core.conf"
+  else
+    echo custom_config_services_dir = /root/.coregui/custom_services >> /etc/core/core.conf
+  fi
 fi
